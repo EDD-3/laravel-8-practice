6d94742e5f1aa268f2d1baf4c4a703ee0b43c82d
Ed <eduramq@gmail.com>
2023-02-21 23:43:42 -0700

    Route filters

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home.index');
+})->name('home.index');
+
+Route::get('/contact', function (){
+    return 'Contact';
+})->name('home.contact');
+
+Route::get('/posts/{id}', function ($id) {
+    return 'Blog post ' . $id;
+})->name('posts.show');
+// ->where([
+//     'id'=> '[0-9]+'
+// ])
+
+Route::get('/recent-posts/{days_ago?}', function ($daysAgo = 20) {
+    return 'Post from ' . $daysAgo;
+})->name('posts.recent.index');
