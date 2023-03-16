99a3b51409736b44b5badbc513de8ab4e773237b
Ed <eduramq@gmail.com>
2023-03-15 21:52:01 -0700

    Added response

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,11 +2,11 @@
 @section('title', 'Blog Post')
     
 @section('content')
+{{-- @each('post.partials.post', $post, 'post') --}}
 
 @forelse ($posts as $key => $post)
-    @include('posts.partials.post')
+    @include('posts.partials.post', [])
 @empty
-
     <div><h1>No posts found!</h1></div> 
 @endforelse
 
