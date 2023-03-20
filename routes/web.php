26eb3a9d7679d37f41c58cf160b159df94ae2cc6
Ed <eduramq@gmail.com>
2023-03-19 23:40:03 -0700

    Added groups,read query parameters and used redirect functions

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -46,29 +47,65 @@ $posts = [
     ]
     ];
 
-Route::get('/posts', function () use ($posts){
-    // compact($posts) === ['posts' => $posts]
-    return view('posts.index', ['posts' => $posts]);
+
+Route::prefix('/posts')->group( function () use ($posts) {
+
+    Route::get('/', function () use ($posts){
+
+        // dd(request()->all());
+        dd((int)request()->input('page',1));
+        dd((int)request()->query('page',5));
+        // compact($posts) === ['posts' => $posts]
+        return view('posts.index', ['posts' => $posts]);
+    })->name('posts');
+    
+    Route::get('{id}', function ($id) use($posts) {
+    
+            abort_if(!isset($posts[$id]), 404);
+    
+        return view('posts.show', ['post' => $posts[$id]]);
+    
+    })->name('posts.show');
+    // ->where([
+    //     'id'=> '[0-9]+'
+    // ])
+    
+
 });
 
-Route::get('/posts/{id}', function ($id) use($posts) {
-
-        abort_if(!isset($posts[$id]), 404);
-
-    return view('posts.show', ['post' => $posts[$id]]);
-
-})->name('posts.show');
-// ->where([
-//     'id'=> '[0-9]+'
-// ])
 
 Route::get('/recent-posts/{days_ago?}', function ($daysAgo = 20) {
     return 'Post from ' . $daysAgo;
 })->name('posts.recent.index');
 
-Route::get('/fun/responses', function () use($posts) {
+
+Route::prefix('/fun')->name('fun.')->group(function() use($posts) {
+    Route::get('responses', function () use($posts) {
     
-    return response($posts, 201)
-    ->header('Content-Type', 'application/json')
-    ->cookie('MY_COOKIE','EDUARDO RAMOS', 3600);
+        return response($posts, 201)
+        ->header('Content-Type', 'application/json')
+        ->cookie('MY_COOKIE','EDUARDO', 3600);
+    })->name('responses');
+    
+    Route::get('redirect', function() {
+        return back();
+    })->name('redirect');
+    
+    Route::get('named-route', function() {
+        return redirect()->route('posts.show', ['id' => 1]);
+    })->name('named-route');
+    
+    Route::get('away', function() {
+        return redirect()->away('https://google.com');
+    })->name('away');
+    
+    Route::get('json', function()  use ($posts) {
+        return response()->json($posts);
+    })->name('json');
+    
+    Route::get('download', function() {
+        return response()->download(public_path('/daniel.jpg', 'GetOutActor.jpg'));
+    })->name('download');
+    
 });
+
