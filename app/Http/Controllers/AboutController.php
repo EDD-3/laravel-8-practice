e2b39d749e033ab8c1755b574ab8ec48134bb475
Ed <eduramq@gmail.com>
2023-03-25 16:01:08 -0700

    Single action controller and use controller

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    //p
+    public function __invoke()
+    {
+        return 'Single';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,9 +24,11 @@ use Illuminate\Support\Facades\Route;
 //     return view('home.contact');
 // })->name('home.contact');
 
-Route::view('/', 'home.index')->name('home.index');
+Route::get('/', [HomeController::class, 'home'])->name('home.index');
 
-Route::view('/contact', 'home.contact')->name('home.contact');
+Route::get('/contact', [HomeController::class, 'contact'])->name('home.contact');
+
+Route::get('/single', AboutController::class);
 
 $posts = [
     1 => [
@@ -69,14 +73,14 @@ Route::prefix('/posts')->group( function () use ($posts) {
     // ->where([
     //     'id'=> '[0-9]+'
     // ])
-    
+
 
 });
 
 
 Route::get('/recent-posts/{days_ago?}', function ($daysAgo = 20) {
     return 'Post from ' . $daysAgo;
-})->name('posts.recent.index');
+})->name('posts.recent.index')->middleware('auth');
 
 
 Route::prefix('/fun')->name('fun.')->group(function() use($posts) {
