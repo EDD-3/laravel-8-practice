cd4dfdfcc09648d39a3a075304636ff37d31fdca
Ed <eduramq@gmail.com>
2023-04-12 23:06:05 -0700

    Added blogpost and comment one to many relationship

---
+++ database/seeders/BlogPostSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\BlogPost;
+use Illuminate\Database\Seeder;
+
+class BlogPostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        BlogPost::factory()->count(10)->create();
+    }
+}
