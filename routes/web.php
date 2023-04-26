7f3915b9f203ed722757fb9d526dbba7fae115e6
Ed <eduramq@gmail.com>
2023-04-26 16:25:05 -0700

    Added Auth resource

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AboutController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostsController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,6 +25,8 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/contact', function (){
 //     return view('home.contact');
 // })->name('home.contact');
+
+Auth::routes();
 
 Route::get('/', [HomeController::class, 'home'])->name('home.index');
 
