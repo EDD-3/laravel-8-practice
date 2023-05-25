efc124ce647bdb10108502712f695ddc12e84c29
Ed <eduramq@gmail.com>
2023-05-24 17:38:00 -0700

    Added comment, blog post and user factory to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\BlogPost;
+use App\Models\Comment;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -16,13 +18,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        //Laravel 8.x <
         //factory(App\User::class)->states('john-doe')-create();
-        User::factory()->newCustomUser()->create();
+        $john = User::factory()->newCustomUser()->create();
         // \App\Models\User::factory(10)->create();
 
         //Laravel 8.x <
         //factory(App\User::class,10)->create();
-        User::factory()->count(20)->create();
+        $users = User::factory()->count(20)->create()->concat([$john]);
+
+        //We use 'make; as to not save the posts before assigning a user id to each post individually
+        $posts = BlogPost::factory()->count(50)->make()->each(function ($post) use ($users) {
+             $post->user_id = $users->random()->id;
+             $post->save();
+        });
+
+        $comments = Comment::factory()->count(150)->make()->each(function ($comment) use ($posts) {
+            $comment->blog_post_id = $posts->random()->id;
+            $comment->save();
+        });
 
 
     }
