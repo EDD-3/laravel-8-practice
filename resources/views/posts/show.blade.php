912e1fe8a4621000a92fe4062575b897979c63db
Ed <eduramq@gmail.com>
2023-05-28 17:20:39 -0700

    Added component alias to app service provider

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,26 +1,30 @@
 @extends('layouts.app')
 @section('title', $post->title)
-    
+
 @section('content')
-    <h1>{{ $post->title }}</h1>
+    <h1>{{ $post->title }}
+        @if (now()->diffInMinutes($post->created_at) <= 20)
+            @badge(['type' => 'primary'])
+                Brand new Post!
+            @endbadge
+
+            {{-- Without using the aliasing in AppServiceProvider --}}
+            {{-- <x-badge type="primary"> Brand new Post! </x-badge> --}}
+        @endif
+
+    </h1>
     <p>{{ $post->content }}</p>
     <p>Added {{ $post->created_at->diffForHumans() }}</p>
 
-@if (now()->diffInMinutes($post->created_at) <= 20)
-    @component('badge', ['type' => 'primary'])
-        Brand new Post!
-    @endcomponent
-@endif
 
-<h4>Comments</h4>
-@forelse($post->comments as $comment)
-    <p>{{ $comment->content }}, </p>
+    <h4>Comments</h4>
+    @forelse($post->comments as $comment)
+        <p>{{ $comment->content }}, </p>
 
-    <p class="text-muted">
-        added {{ $comment->created_at->diffForHumans() }}
-    </p>
-@empty
-<p>No comments yet!</p>
-
-@endforelse
+        <p class="text-muted">
+            added {{ $comment->created_at->diffForHumans() }}
+        </p>
+    @empty
+        <p>No comments yet!</p>
+    @endforelse
 @endsection
