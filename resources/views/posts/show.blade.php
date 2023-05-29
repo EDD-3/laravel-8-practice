d43ae2e0e7e0664bc744c52529fece9f52aeb650
Ed <eduramq@gmail.com>
2023-05-28 17:26:38 -0700

    Used conditional rendering in laravel components

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,15 +3,12 @@
 
 @section('content')
     <h1>{{ $post->title }}
-        @if (now()->diffInMinutes($post->created_at) <= 20)
-            @badge(['type' => 'primary'])
+            @badge(['show' => now()->diffInMinutes($post->created_at) <= 600])
                 Brand new Post!
             @endbadge
 
             {{-- Without using the aliasing in AppServiceProvider --}}
             {{-- <x-badge type="primary"> Brand new Post! </x-badge> --}}
-        @endif
-
     </h1>
     <p>{{ $post->content }}</p>
     <p>Added {{ $post->created_at->diffForHumans() }}</p>
